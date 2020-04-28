59cd6f2f26965d28076c590d2f070ff4742c7983
Daniel de Lima <dlima@tre-pa.jus.br>
2020-04-28 20:39:58 -0300

    Fim da aula 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text('Bem vindo ao bytebank', textDirection: TextDirection.ltr,));
+void main() => runApp(
+    Column(
+      children: <Widget>[
+        Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+        ),
+        Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              'Craft beautiful UIs',
+              textDirection: TextDirection.ltr,
+            ),
+          ],
+        ),
+      ],
+    )
+    );
